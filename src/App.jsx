58fba5848d94683e832ff402004443c5e02d72c5
MStrelnikova <hashitomada@gmail.com>
2026-06-15 20:25:59 +0200

    header added + styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import Home from "./pages/home";
 
 function App() {
-  
   return (
     <>
-      
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css";
+import Header from "../../components/header";
+
+function Home() {
+  return 
+  (
+    <div className={styles.homeContainer}>
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
