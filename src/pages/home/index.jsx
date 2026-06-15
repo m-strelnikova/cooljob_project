d20a0e979bfd3cdb81118ff8abf14a6a0aa90140
MStrelnikova <hashitomada@gmail.com>
2026-06-15 21:04:34 +0200

    adding  main to home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,14 @@
 import styles from "./styles.module.css";
 import Header from "../../components/header";
 // import CategoryCardList from "../../components/categoryCardList";
+import Main from "../../components/main";
 
 function Home() {
   return 
   (
     <div className={styles.homeContainer}>
       <Header />
+      <Main />
     </div>
   );
 }
