600217ff5b02afe25b330721f6f8559759bcd8cb
MStrelnikova <hashitomada@gmail.com>
2026-06-17 23:39:09 +0200

    added Searchbar and hover-effects

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -3,6 +3,8 @@ import HelpFindWork from '../helpFindWork';
 import SearchBar from '../searchBar';
 import CategoryCardList from '../categoryCardList';
 import VacancyCardList from '../vacancyCardList';
+import figure from "../../assets/icons/main_deco_form.svg";
+
 
 function Main() {
     return (<>
@@ -14,6 +16,9 @@ function Main() {
             <div className={style.inputbar}>
                 <SearchBar />
             </div>
+            <svg className={style.main_deco_figure} width="895" height="708" viewBox="0 0 895 708" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M43.8426 490.462C120.298 425.63 204.227 370.166 293.844 325.251L913.427 14.7242C1037.76 -47.5892 1160.71 101.221 1076.07 211.569L763.861 618.58C741.03 648.344 706.534 666.893 669.113 669.526L132.164 707.307C13.4174 715.662 -46.9492 567.451 43.8426 490.462Z" fill="#92C9FC" />
+</svg>
 
         </div>
         <HelpFindWork />
--- src/components/searchBar/index.jsx
+++ src/components/searchBar/index.jsx
@@ -1,6 +1,25 @@
 import style from './styles.module.css';
+import search_loc from '../../assets/icons/search_location.svg';
+import search_job from '../../assets/icons/search_job.svg';
+import divide_line from '../../assets/icons/divide_line.svg'
 
 function SearchBar() {
+    return (
+        <div className={style.searchbar_box}>
+            <div className={style.search_job}>
+                <img src={search_job} alt="search icon" />
+                <span>Должность или компания</span>
+            </div>
+            <div className={style.search_loc}>
+                <img src={divide_line} className={style.divide_line}/>
+                <img src={search_loc} alt="location icon" />
+                <span>Город, Страна</span>
+            </div>
+            <div>
+                <button>Поиск</button>
+            </div>
+        </div>
+    )
 
 }
 export default SearchBar;
